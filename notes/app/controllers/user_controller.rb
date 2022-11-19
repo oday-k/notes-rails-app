e550d18855059f6cfaf74eb44667bb009a0198dd
oday aldweik <oday@jawaker.com>
2022-11-19 21:03:40 +0300

    feat(user): return something on creation

--- notes/app/controllers/user_controller.rb
+++ notes/app/controllers/user_controller.rb
@@ -1,11 +1,14 @@
 class UserController < ApplicationController
   def signup
     User.create(user_params)
+
+    render :head, status: :created
   end
 
   private
 
   def user_params
+    # Question: is there a way to enforce param types
     params.require(:user).permit(:name, :email, :password)
   end
 end
