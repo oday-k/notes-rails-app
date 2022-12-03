4864ce1e67e16b6b0909af49f8f24ed34698d22c
oday aldweik <oday@jawaker.com>
2022-12-04 00:17:24 +0300

    feat: add jwt denylist table

--- notes/app/models/user.rb
+++ notes/app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         :validatable,
+         jwt_revocation_strategy: JwtDenylist
+
+  def authenticate(password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
+
   has_many :notes
-  has_secure_password
 end
