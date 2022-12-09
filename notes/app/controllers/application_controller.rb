e730ec9a8e2ab5287f1f8c9bd5959253d506ad1c
oday aldweik <oday@jawaker.com>
2022-12-09 19:54:18 +0300

    feat: handle `unauthticated` and `not found` erros gracefully

--- notes/app/controllers/application_controller.rb
+++ notes/app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::API
   include ActionController::MimeResponds
 
   respond_to :json
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      render json: { error: 'you need to login' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def record_not_found
+    render json: { error: 'record not found' }, status: :not_found
+  end
 end
